1a55ee9aa5b84a23ac42080f78a80ea4316da665
SVladimir <semennikovvladimir@gmail.com>
2021-07-21 20:33:27 +0300

    Task 2.1.2 v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("first: " + (bean == bean2));
+        System.out.println("==========================================");
+
+        Cat newBean = (Cat) applicationContext.getBean("cat");
+        Cat newBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(newBean.getMessage());
+        System.out.println("second: " + (newBean == newBean2));
     }
 }
